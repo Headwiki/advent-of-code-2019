b6dcce638052c9dd5bf7ae33576c2290e4a42102
Henrik Hedlund Andersen <heheand@gmail.com>
2019-12-01 19:56:22 +0100

    part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+
+fn main() -> io::Result<()> {
+    let file = File::open("input")?;
+    let reader = BufReader::new(file);
+    let mut sum = 0;
+
+    for line in reader.lines() {
+        sum += fuel_required(line?.parse::<f32>().unwrap())
+    }
+
+    println!("{}", sum);
+    Ok(())
+}
+
+fn fuel_required(mass: f32) -> u32 {
+    ((mass / 3.0).floor() - 2.0) as u32
 }
