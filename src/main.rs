7609cc12100381c63711c9c304fffcfdea2d4e32
Henrik Hedlund Andersen <heheand@gmail.com>
2019-12-01 21:45:27 +0100

    part 2

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,25 @@ fn main() -> io::Result<()> {
     let mut sum = 0;
 
     for line in reader.lines() {
-        sum += fuel_required(line?.parse::<f32>().unwrap())
+        sum += total_fuel(line?.parse::<u32>().unwrap())
     }
 
     println!("{}", sum);
     Ok(())
 }
 
-fn fuel_required(mass: f32) -> u32 {
-    ((mass / 3.0).floor() - 2.0) as u32
+fn fuel_required(mass: u32) -> u32 {
+    (mass as f32 / 3.0).floor() as u32 - 2
+}
+
+fn total_fuel(mut mass: u32) -> u32{
+    let mut total = 0;
+
+    while mass > 5 {
+        println!("Total: {}, Mass: {}", total, mass);
+        mass = fuel_required(mass);
+        total += mass;
+    }
+
+    total
 }
