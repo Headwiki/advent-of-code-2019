029005e95a71939d7a372d5a1db5b7e17687c0d3
Henrik Hedlund Andersen <Henrik.Hedlund.Andersen@dgi.no>
2019-12-02 14:22:46 +0100

    parse input to vec<u32>

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,17 @@ fn main() -> io::Result<()> {
     let file = File::open("input")?;
     let reader = BufReader::new(file);
 
+    let mut int_codes: Vec<u32> = Vec::new();
+
     for line in reader.lines() {
-        println!("{}", line?);
+        int_codes = line.unwrap().split(',').map(|i| i.parse::<u32>().unwrap()).collect();
     }
 
+    println!("{:?}", int_codes);
+
     Ok(())
+}
+
+fn execute_intcode (intcodes: Vec<u32>) -> Vec<u32> {
+    unimplemented!();
 }
