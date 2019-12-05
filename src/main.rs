d7bc3d86627334f1219b4db777f8332f2ac161c9
Henrik Hedlund Andersen <heheand@gmail.com>
2019-12-05 18:15:07 +0100

    part 1

--- src/main.rs
+++ src/main.rs
@@ -5,57 +5,124 @@ fn main() -> io::Result<()> {
     let file = File::open("input")?;
     let reader = BufReader::new(file);
 
-    let mut intcodes: Vec<u32> = Vec::new();
+    let mut intcode: Vec<i32> = Vec::new();
 
     for line in reader.lines() {
-        intcodes = line.unwrap().split(',').map(|i| i.parse::<u32>().unwrap()).collect();
+        intcode = line.unwrap().split(',').map(|i| i.parse::<i32>().unwrap()).collect();
     }
 
-    //println!("{:?}", execute_intcode(intcodes));
-    println!("{:?}", brute_intcodes(&intcodes));
+    execute_intcode(intcode);
+    //println!("{:?}", brute_intcodes(&intcodes));
     Ok(())
 }
 
-fn execute_intcode (mut intcodes: Vec<u32>) -> Vec<u32> {
-    let mut intcode_op_pos = 0;
+fn execute_intcode (mut intcode: Vec<i32>) -> Vec<i32> {
+    let mut instruction_position = 0;
 
-    // While operation code not eqult to 99 - 'halt'
     loop{
-        match intcodes[intcode_op_pos] {
-            1 => {
-                let first_pos = intcodes[intcode_op_pos+1] as usize;
-                let second_pos = intcodes[intcode_op_pos+2] as usize;
-                let result_pos = intcodes[intcode_op_pos+3] as usize;
+        let instruction = intcode[instruction_position];
 
-                intcodes[result_pos] = intcodes[first_pos] + intcodes[second_pos];
+        // parse instruction
+        let instruction_vec: Vec<u32> = 
+            instruction.to_string().
+                chars().
+                map(|x| x.to_digit(10).unwrap())
+                .collect();
+        
+        let instruction_length = instruction_vec.len();
+        
+        let operation_mode = instruction % 100;
+
+        let mut parameter_mode_one = 0;
+        let mut parameter_mode_two = 0;
+
+        if instruction_length == 4 {
+            parameter_mode_two = instruction_vec[0];
+            parameter_mode_one = instruction_vec[1];
+
+        } else if instruction_length == 3 {
+            parameter_mode_one = instruction_vec[0];
+        }
+
+        // Match operation mode
+        match operation_mode {
+            1 => {
+                let mut first_pos = 0;
+                let mut second_pos = 0;
+                let result_pos = intcode[instruction_position+3] as usize;
+
+                if parameter_mode_one == 0 {
+                    first_pos = intcode[intcode[instruction_position+1] as usize];
+                } else {
+                    first_pos = intcode[instruction_position+1];
+                }
+
+                if parameter_mode_two == 0 {
+                    second_pos = intcode[intcode[instruction_position+2] as usize];
+                } else {
+                    second_pos = intcode[instruction_position+2];
+                }
+
+                intcode[result_pos] = first_pos + second_pos;
             },
             2 => {
-                let first_pos = intcodes[intcode_op_pos+1] as usize;
-                let second_pos = intcodes[intcode_op_pos+2] as usize;
-                let result_pos = intcodes[intcode_op_pos+3] as usize;
+                let mut first_pos = 0;
+                let mut second_pos = 0;
+                let result_pos = intcode[instruction_position+3] as usize;
 
-                intcodes[result_pos] = intcodes[first_pos] * intcodes[second_pos];
+                if parameter_mode_one == 0 {
+                    first_pos = intcode[intcode[instruction_position+1] as usize];
+                } else {
+                    first_pos = intcode[instruction_position+1];
+                }
+
+                if parameter_mode_two == 0 {
+                    second_pos = intcode[intcode[instruction_position+2] as usize];
+                } else {
+                    second_pos = intcode[instruction_position+2];
+                }
+
+                intcode[result_pos] = first_pos * second_pos;
             },
-            99 => { return intcodes },
+            3 => {
+                let first_pos = intcode[instruction_position+1] as usize;
+                let mut input = String::new();
+                println!("Enter input: ");
+
+                std::io::stdin().lock().read_line(&mut input).unwrap();
+                input.pop();    // remove trailing newline
+                input.pop();
+                intcode[first_pos] = input.parse::<i32>().unwrap();
+            },
+            4 => {
+                let first_pos = intcode[instruction_position+1] as usize;
+                println!("Output: {}", intcode[first_pos]);
+            },
+            99 => { return intcode },
             _ => { panic!("Unknown operation code!"); }
         }
-        intcode_op_pos += 4;
+
+        if (operation_mode == 3) ||(operation_mode == 4) {
+            instruction_position += 2;
+        } else {
+            instruction_position += 4;
+        }
     }
 
 }
 
-fn brute_intcodes(intcodes: &Vec<u32>) -> Vec<u32> {
-    let mut intcodes = intcodes.clone();
+/* fn brute_intcodes(intcode: &Vec<i32>) -> Vec<i32> {
+    let mut intcode = intcode.clone();
     for x in 0..100 {
-        intcodes[1] = x;
+        intcode[1] = x;
         for y in 0..100 {
-            intcodes[2] = y;
+            intcode[2] = y;
 
-            let result = execute_intcode(intcodes.clone());
+            let result = execute_intcode(intcode.clone());
             if result[0] == 19690720 {
                 return result
             }
         }
     }
     vec![0]
-}
+} */
